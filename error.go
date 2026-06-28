2f0bacfd5a40d54fd81b09321dc76b73f24d00cb
krelinga <Krelinga@gmail.com>
2026-06-28 18:59:05 -0500

    Create error.go
    Created on mobile, so no compiling or testing has happened yet.

---
+++ error.go
@@ -0,0 +1,24 @@
+package diff
+
+// Error is used for the error return of Differ.Diff().
+//
+// It wraps the original error and also contains the path at which the error happened.
+type Error struct {
+	Path Path
+	Err error
+}
+
+func NewError(p Path, e error) Error {
+	return Error{
+		Path: p,
+		Err: e,
+	}
+}
+
+func (e Error) Error() string {
+	return fmt.Sprintf("%s: %s", e.Path, e.Err.Error())
+}
+
+func (e Error) Unwrap() error {
+	return e.Err
+}
