0ba65e2ef3a9d68325d3f1e5ee2562a72508ad06
Andy Kreling <krelinga@gmail.com>
2026-06-30 17:05:01 +0000

    Counter implementation of Reporter.

---
+++ counter.go
@@ -0,0 +1,27 @@
+package diff
+
+// Counter is a Reporter that counts the number of differences, left-only elements, and right-only elements.
+type Counter struct {
+	NumDifferent int
+	NumLeftOnly  int
+	NumRightOnly int
+}
+
+func (c *Counter) Different() {
+	c.NumDifferent++
+}
+
+func (c *Counter) LeftOnly(_ Key, _ any) {
+	c.NumLeftOnly++
+}
+
+func (c *Counter) RightOnly(_ Key, _ any) {
+	c.NumRightOnly++
+}
+
+func (_ *Counter) Push(_ Key, _, _ any) {}
+func (_ *Counter) Pop()                 {}
+
+func (c *Counter) Total() int {
+	return c.NumDifferent + c.NumLeftOnly + c.NumRightOnly
+}
